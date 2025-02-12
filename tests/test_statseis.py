86e7f0e0b8412629c3d631a75c1eaafc145721b1
Russell Azad Khan <russellazadkhan@gmail>
2025-02-12 16:44:03 +0000

    Fixed module import error during testing

--- tests/test_statseis.py
+++ tests/test_statseis.py
@@ -1,2 +1,16 @@
-import statseis.statseis
+import pytest
+import numpy as np
+import statsmodels.api as sm
+
+@pytest.fixture
+def sample_numbers():
+    return [0.1,0.2,0.3]
+
+from statseis.statseis import gamma_law_MLE
+
+print(gamma_law_MLE([0.1,0.2,0.3]))
+
+def test_gamma_law_MLE(sample_numbers):
+    assert gamma_law_MLE(sample_numbers) == 9.998000000000001
+
 
