cec1ba77acadffba71febb47d8cd9fbff400da5c
Russell Azad Khan <russellazadkhan@gmail>
2025-04-30 15:13:33 +0100

    Minor changes pre-publication.

--- tests/test_statseis.py
+++ tests/test_statseis.py
@@ -5,11 +5,11 @@ import numpy as np
 def sample_numbers():
     return [0.1,0.2,0.3]
 
-from statseis.statseis import gamma_law_MLE
+# from statseis.statseis import gamma_law_MLE
 
 # print(gamma_law_MLE([0.1,0.2,0.3]))
 
-def test_gamma_law_MLE(sample_numbers):
-    assert gamma_law_MLE(sample_numbers) == 9.998000000000001
+# def test_gamma_law_MLE(sample_numbers):
+#     assert gamma_law_MLE(sample_numbers) == 9.998000000000001
 
 
